e6ed077ec1c38908ff17f8ba24545b7bb7f160a5
SajeewaJayarathne <sajeewagj@gmail.com>
2022-02-14 21:21:52 +0530

    SJ|Adding privacy policy & terms of use

--- www/js/main.js
+++ www/js/main.js
@@ -10,38 +10,39 @@ $(document).scroll(function () {
 
 /* Popup */
 // Get the modal
+const ModalPopupTypes = {
+    1: 'modalPrivacyPolicy',
+    2: 'modalTermsOfUse',
+    /*3: 'modalContentCEO',
+    4: 'modalContentAbout'*/
+};
+
 let modal = document.getElementById("myModal");
-let modalContentCEO = document.getElementById("modalContentCEO");
-let modalContentAbout = document.getElementById("modalContentAbout");
+let modalContentElem;
 
 // When the user clicks the button, open the modal
-function readMoreCEOMsg () {
+function openModalPopup(mode) {
     modal.style.display = "block";
-    modalContentCEO.style.display = "block";
+
+    modalContentElem = document.getElementById(ModalPopupTypes[mode]);
+    modalContentElem.style.display = "block";
 }
 
-function readMoreAbout () {
-    modal.style.display = "block";
-    modalContentAbout.style.display = "block";
-}
-
-function closeModalPopup () {
+function closeModalPopup() {
     modal.style.display = "none";
-    modalContentCEO.style.display = "none";
-    modalContentAbout.style.display = "none";
+    modalContentElem.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target === modal) {
         modal.style.display = "none";
-        modalContentCEO.style.display = "none";
-        modalContentAbout.style.display = "none";
+        modalContentElem.style.display = "none";
     }
 }
 
-function clearForm () {
-    for(const form of document.getElementsByTagName('form')) {
+function clearForm() {
+    for (const form of document.getElementsByTagName('form')) {
         form.reset();
     }
 }
